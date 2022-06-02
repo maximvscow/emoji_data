86ee3d0fdc31b1d43d08d92dedb0418dd9b6cae9
Maxim Vasyukov <vasyuckoff.m@yandex.ru>
2022-06-02 20:29:54 +0300

    parcer to mongo + old data

--- vk_parce/parcer.py
+++ vk_parce/parcer.py
@@ -1,9 +1,9 @@
 import vk_api
 import re
-import pandas as pd
 import time
 import datetime
 import json
+from pymongo import MongoClient
 
 
 def get_owner_id(link):
@@ -19,7 +19,7 @@ def get_posts(link, begin_date):
     owner = get_owner_id(link)
     posts_list = list()
     end_date = date_begin + 86400
-    wall = vk.wall.get(owner_id=int(owner), count=5, v=5.131)
+    wall = vk.wall.get(owner_id=int(owner), count=100, v=5.131)
     for post in wall['items']:
         if begin_date < post['date'] < end_date:
             post_data = dict()
@@ -67,33 +67,60 @@ def get_replies(link, comment_id, post_id):
     return replies
 
 
+def insert_document(collection, data):
+    return collection.insert_one(data).inserted_id
+
+
 if __name__ == "__main__":
-    gr_urls = [r"https://vk.com/true_lentach"]
+    gr_urls = [r"https://vk.com/barneos22", r"https://vk.com/incident22", r"https://vk.com/chb_brn",
+               r"https://vk.com/leftbiysk", r"https://vk.com/altaikrai_ldpr"]
     token = "ca62f3d9ca62f3d9ca62f3d9c7ca1ea088cca62ca62f3d9a8077c79d0ec187ff848d27c"
     vk_session = vk_api.VkApi()
     vk_session.token = {'access_token': token, 'expires_in': 0}
     vk = vk_session.get_api()
-    print('**Парсер сообществ социальной сети ВКонтакте (VK) \n')
+    client = MongoClient('localhost', 27017)
+    flag = True
+    print('\n')
+    print('**Парсер записей и комментариев к ним со стены сообществ социальной сети ВКонтакте (VK) \n')
+    print('  ____     _____     _____     _   _     _____')
+    print(' /  __\\   |_   _|   |  _  |   | | | |   /  _  \\')
+    print(' | |        | |     | |_| |   | |_| |   | |_| |')
+    print(' | |        | |     |  ___|   |  _  |   |  _  |')
+    print(' | |___     | |     | |___    | | | |   | | | |')
+    print(' \\____/     |_|     |_____|   |_| |_|   |_| |_|   v 1.0')
+    print('')
+    print('©Академия ФСО России \n')
+    db_name = input("Введите название базы данных MongoDB: ")
+    series_name = input("Введите название коллекции MongoDB: ")
+    print('')
+    db = client[db_name]
+    series_collection = db[series_name]
     print('Сообщества, из которых собираются данные:')
     for url in gr_urls:
         print(url)
-    check = input("Вам необходимо добавить новое сообщество? (да/нет): ")
-    try:
+    print('\n')
+    while flag:
+        check = input("Вам необходимо добавить новое сообщество? (да/нет): ")
         if check == "да":
             new_url = input("Введите ссылку на сообщество: ")
             gr_urls.append(new_url)
-    except Exception as e:
-        
-
-
+            print('\n Новый список сообществ:')
+            for url in gr_urls:
+                print(url)
+            print('')
+        elif check == "нет":
+            flag = False
+        else:
+            print('Некорректный ответ! Попробуйте снова!')
     date = input("Введите дату, за которую необходимо собрать сообщения со стены,"
                  " в формате %d/%m/%Y (например 01/01/2022): ")
     date_begin = int(datetime.datetime.strptime(date, "%d/%m/%Y").timestamp())
+    print('Начинаем сбор данных...')
     for url in gr_urls:
         all_posts = get_posts(url, date_begin)
         full_posts_data = get_comments(url, all_posts)
-        for post in full_posts_data:
-            post = json.dumps(post)
-            json_post = json.loads(post)
-            print(json_post)
-
+        for post_full in full_posts_data:
+            post_full = json.dumps(post_full)
+            json_post = json.loads(post_full)
+            insert_document(series_collection, json_post)
+        print('Собраны записи для сообщества: ' + str(url))
